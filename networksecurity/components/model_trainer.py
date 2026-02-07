cc4543784dde86d2f74498a3da52192985a3e7f5
Nihal108-bi <nihaljaisawal1@gmail.com>
2026-02-07 22:27:43 +0530

    Dags hub and Final model folder created

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -22,6 +22,8 @@ from sklearn.ensemble import (
     RandomForestClassifier,
 )
 import mlflow
+import dagshub
+dagshub.init(repo_owner='Nihal108-bi', repo_name='AI-Powered-Network-Security-Detection-Pipeline-End-to-End-MLOps-', mlflow=True)
 
 class ModelTrainer:
     def __init__(self,model_trainer_config:ModelTrainerConfig,data_transformation_artifact:DataTransformationArtifact):
@@ -105,8 +107,10 @@ class ModelTrainer:
 
         Network_Model=NetworkModel(preprocessor=preprocessor,model=best_model)
         save_object(self.model_trainer_config.trained_model_file_path,obj=NetworkModel)
+        
+        save_object("final_model/model.pkl",best_model)
 
-        ## model trainer artifact
+        ## Model trainer artifact
         model_trainer_artifact=ModelTrainerArtifact(trained_model_file_path=self.model_trainer_config.trained_model_file_path,
                              train_metric_artifact=classification_train_metric,
                              test_metric_artifact=classification_test_metric
